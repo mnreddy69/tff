a1d4c1332c27c51c9fd6611d812b576bf8ed89cf
Nageshwar Reddy Marella <nagesh.marella@gmail.com>
2021-05-13 15:18:23 -0400

    My First Commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA5KOY4NEOEZ4VCKGU"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
